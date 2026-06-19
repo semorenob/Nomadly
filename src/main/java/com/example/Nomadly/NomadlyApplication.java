cc4bbd9428b595d9d0990387eea7fcc9343fc3c6
Matías Moreno <215863536+semorenob@users.noreply.github.com>
2026-06-19 17:59:08 -0400

    proyecto base

---
+++ src/main/java/com/example/Nomadly/NomadlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nomadly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomadlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Nomadly/NomadlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nomadly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
