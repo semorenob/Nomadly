f426d0cc35d6b7add421497f91a391b745ef5f27
CARLOSRAMIREZ2006 <carl.ramirezg@duocuc.cl>
2026-06-22 22:52:59 -0400

    Ultimas modificaciones para dejar Swagger listo, tuvimos que sacar Eureka.

--- src/main/java/com/example/Nomadly/NomadlyApplication.java
+++ src/main/java/com/example/Nomadly/NomadlyApplication.java
@@ -2,10 +2,10 @@ package com.example.Nomadly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+//@EnableEurekaServer
 public class NomadlyApplication {
 
 	public static void main(String[] args) {
