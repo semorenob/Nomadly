688b09fd8ded62f48099afeb9bf52f76d2945a85
F-brizzio <fa.mego@duocuc.cl>
2026-06-24 17:45:03 -0400

    fix emplementacion eureka

--- src/main/java/com/example/Nomadly/NomadlyApplication.java
+++ src/main/java/com/example/Nomadly/NomadlyApplication.java
@@ -2,9 +2,11 @@ package com.example.Nomadly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaServer
 @EnableFeignClients
 public class NomadlyApplication {
 
