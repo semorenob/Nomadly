d21c26a15c04777eb7b15a29b44dae9fcf826e22
CARLOSRAMIREZ2006 <carl.ramirezg@duocuc.cl>
2026-06-25 19:55:29 -0400

    Ojala funcione postman con esto (POM, Application.yml, NomadlyApplication.java).

--- src/main/java/com/example/Nomadly/NomadlyApplication.java
+++ src/main/java/com/example/Nomadly/NomadlyApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -14,4 +17,11 @@ public class NomadlyApplication {
 		SpringApplication.run(NomadlyApplication.class, args);
 	}
 
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+		return http.build();
+	}
 }
